433f24b6ab6463f0ff1d0910591c880571fc7f91
fdzr <fzamora.reina@gmail.com>
2022-06-19 14:37:12 -0400

    adding repo

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+
+// import {
+//     BrowserRouter as Router,
+//     Routes,
+//     Route,
+// } from "react-router-dom";
+
+const App = () => {
+    return (
+        <>
+            <Navbar />
+            <Hero />
+        </>
+    )
+}
+
+export default App;
