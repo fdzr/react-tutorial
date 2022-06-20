52d2b84dd91a041026cb50c02a46b0a985963e14
fdzr <fzamora.reina@gmail.com>
2022-06-19 23:16:54 -0400

    adding functionalities

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import About from './components/About';
+import Support from './components/Support';
+import AllInOne from './components/AllInOne';
+import Pricing from './components/Pricing';
+import Footer from './components/Footer';
 
 // import {
 //     BrowserRouter as Router,
@@ -13,6 +18,11 @@ const App = () => {
         <>
             <Navbar />
             <Hero />
+            <About />
+            <Support />
+            <AllInOne/>
+            <Pricing />
+            <Footer />
         </>
     )
 }
